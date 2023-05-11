de598336f0c1a84ebc07473108f44d0d16d5083e
Caleb Oki <caleb.oki@ibm.com>
2023-05-11 14:03:23 -0400

    added error messages to form fields

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -7,6 +7,20 @@
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
         <form class="space-y-6" @submit.prevent="submitForm">
+          <div>
+            <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
+            <div class="mt-1">
+              <input
+                id="name"
+                name="name"
+                type="text"
+                v-model="form.name"
+                required
+                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+              <p class="text-red-500 text-xs italic">{{ errors.name[0] }}</p>
+            </div>
+          </div>
           <!-- Email input -->
           <div>
             <label for="email" class="block text-sm font-medium text-gray-700">
@@ -21,6 +35,7 @@
                 required
                 class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
+              <p class="text-red-500 text-xs italic">{{ errors.email[0] }}</p>
             </div>
           </div>
 
@@ -36,6 +51,7 @@
                 required
                 class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
+              <p class="text-red-500 text-xs italic">{{ errors.password[0] }}</p>
             </div>
           </div>
 
@@ -52,6 +68,7 @@
                 required
                 class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
+              <p class="text-red-500 text-xs italic">{{ errors.confirmPassword[0] }}</p>
             </div>
           </div>
 
@@ -110,21 +127,55 @@
 
 <script lang="ts">
 import { ref } from 'vue'
+import axios from 'axios' // import axios library
 
 export default {
   setup() {
     const form = ref({
+      name: '',
       email: '',
       password: '',
-      confirmPassword: '',
+      confirmPassword: ''
     })
 
-    const submitForm = () => {
-      // Add your sign-up logic here
+    const errors = ref({ name: '', email: '', password: '', confirmPassword: '' });
+
+    const submitForm = async () => {
+
+      // if (form.value.password !== form.value.confirmPassword) {
+      //   // handle the passwords do not match case
+      //   console.error('Passwords do not match')
+      //   return
+      // }
+
+      try {
+        const response = await axios.post(`${import.meta.env.VITE_API_SERVER_URL}/api/register`, {
+          name: form.value.name,
+          email: form.value.email,
+          password: form.value.password,
+          password_confirmation: form.value.confirmPassword,
+        })
+
+        // handle the successful case, for instance, redirect to login page
+        console.log(response.data)
+      } catch (error) {
+        // handle the error case, for instance, show error message
+        console.error(error)
+        if (error.response && error.response.data.errors) {
+          // handle form validation errors
+          errors.value.name = error.response.data.errors.name || '';
+          errors.value.email = error.response.data.errors.email || '';
+          errors.value.password = error.response.data.errors.password || '';
+          errors.value.confirmPassword = error.response.data.errors.confirmPassword || ''
+
+          console.log(errors.value.confirmPassword)
+        }
+      }
     }
 
     return {
       form,
+      errors,
       submitForm
     }
   }
